2fc2ea82ed3941de1e09aa23a395e492a0ae9b95
vagrant <vagrant@localhost.localdomain>
2016-10-22 06:29:47 +0000

    First commit

---
+++ tests/GumballMachineTest.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../GumballMachine.php';
+
+class GumballMachineTest extends PHPUnit_Framework_TestCase
+{
+	public $gumballMachineInstance;
+
+	public function setUp()
+	{
+		$this->gumballMachineInstance = new GumballMachine();
+	}
+
+	public function testIfWheelWorks()
+	{
+		//suppose we have 100 gumballs..
+		$this->gumballMachineInstance->setGumballs(100);
+
+		//..and we turn the wheel once..
+		$this->gumballMachineInstance->turnWheel();
+
+		//..we should now have 99 gumballs remaining in the machine
+		$this->assertEquals(99, $this->gumballMachineInstance->getGumballs());
+	}
+}
